71418ff7dc3a5f39db8bde6a791d8397abfc399b
AK <abdelkader.kettal@paymium.com>
2018-03-23 11:44:36 +0100

    working version for signTx :)

--- src/address-generation/eth.js
+++ src/address-generation/eth.js
@@ -1,127 +1,194 @@
 import AppEth from "@ledgerhq/hw-app-eth";
 import TransportU2F from "@ledgerhq/hw-transport-u2f";
 import TransportNodeHid from "@ledgerhq/hw-transport-node-hid";
-//import EthereumTx from 'ethereumjs-tx'
 const EthereumTx = require('ethereumjs-tx')
+import {API} from '../etherscan';
 
+// see https://github.com/satoshilabs/slips/blob/master/slip-0044.md
 const BASE_PATH = "44'/60'/0'/0/"
 
-let args = require('minimist')(process.argv.slice(2))
+class EthWallet {
 
-let operation = args['op']
+  async execute() {
+    let args = require('minimist')(process.argv.slice(2))
 
-switch(operation) {
-  case "gen-addr":
-    let nb_addresses = args['nb'] || 10
-    genAddresses(nb_addresses)
-    break
-  case "sign-tx":
-    let addressPath = args['addr-path']
-    let txParams    = args['tx-params']
-    signTransaction(addressPath, txParams)
-    break
-  default:
-    printUsage(process.argv);
-}
+    switch(args['op']) {
+      case "gen-addr":
+        this.nb_addresses = args['nb'] || 10
+        this.genAddresses()
+        break
 
-async function getClient() {
-  let eth;
-  try {
-    let transport = await TransportNodeHid.create()
-    return new AppEth(transport)
-  } catch(e) {
-    console.log(e)
+      case "get-addr-from-path":
+        const path    = args['path']
+        const address = await this.getAddress(path)
+        this.log('Address', address)
+        break
+
+      case "sign-tx":
+        let addressPath = args['addr-path']
+        let txParams  = args['tx-params']
+        this.signTransaction(addressPath, txParams)
+        break
+      default:
+        printUsage(process.argv);
+    }
   }
-}
 
-async function genAddresses(nb_addresses) {
-  let eth = await getClient()
-
-  // CSV header
-  console.log("path, publicKey, address")
-
-  for (let index of Array(nb_addresses).keys()) {
-    let path = `${BASE_PATH}${index}`
-
-    await eth.getAddress(path).then(function(result) {
-      let publicKey = result.publicKey
-      let address   = result.address
-      console.log(`${index},"${path}", ${publicKey}, ${address}`);
-
-    }).catch(function(err) {
-      console.log(err.name)
-      console.log(err.message)
-      console.log(err.statusCode)
-      console.log(err.statusText)
-      console.log('-')
-    });
+  async getClient() {
+    if(!this.eth) {
+      console.log('------- CREATING CLIENT --------')
+      try {
+        let transport = await TransportNodeHid.create()
+        this.eth = new AppEth(transport)
+      } catch(e) {
+        this.log('getClient error', e)
+        throw e
+      }
+    } else
+      console.log('------- USING CLIENT --------')
+    
+    return this.eth
   }
-}
 
-function generateRawTransaction(txParams) {
+  async getAddress(path) {
+    try {
+      let eth = await this.getClient()
+      return await eth.getAddress(path)
+    } catch(err) {
+      this.log('GetAddress Error', err)
+    }
+  }
+
+  async genAddresses() {
+    try {
+      let eth = await this.getClient()
+
+      // CSV header
+      console.log("path, publicKey, address")
+
+      for (let index of Array(this.nb_addresses).keys()) {
+        let path = `${BASE_PATH}${index}`
+
+          await eth.getAddress(path).then(function(result) {
+            let publicKey = result.publicKey
+            let address   = result.address
+            console.log(`${index},"${path}", ${publicKey}, ${address}`);
+
+          }).catch(function(err) {
+            console.log(err.name)
+            console.log(err.message)
+            console.log(err.statusCode)
+            console.log(err.statusText)
+            console.log('-')
+          });
+      }
+    } catch(err) {
+      this.log('GenAddresses Error', err)
+    }
+  }
+
+  createTx(txParams) {
+    return new EthereumTx(txParams)
+  }
+
+  async signTransaction(addressPath, txParamsStr) {
+    try {
+      let eth  = await this.getClient()
+
+      let from = await this.getAddress(addressPath)
+
+      let txParams = JSON.parse(txParamsStr)
+      txParams['gasPrice'] = await this.getGasPrice()
+      txParams['nonce']    = await this.getNonceForAddress(from)
+      this.log('TX Params', txParams)
+
+      let tx = this.createTx(txParams)
+      // HACK (ethereumjs-tx library bug) !!!
+      tx.v = Buffer.from([1])
+
+      let signature = await eth.signTransaction(addressPath, tx.serialize().toString('hex'))
+      signature['v'] = '0x' + signature['v']
+      signature['r'] = '0x' + signature['r']
+      signature['s'] = '0x' + signature['s']
+
+      let signedTxParams = {...txParams, ...signature}
+
+      let signedTx = this.createTx(signedTxParams)
+      this.log('Serialized Signed TX', signedTx.serialize().toString('hex'))
+
+      let senderAddress = signedTx.getSenderAddress().toString('hex')
+      this.log('Sender address', senderAddress)
+
+    } catch(err) {
+      this.log('TRANSACTION NOT SIGNED', err)
+    }
+  }
+
+  async getNonceForAddress(address) {
+    try {
+      const api = this.getApi()
+      return await api.getTransactionCount(address)
+    } catch(err) {
+      this.log('getNonceForAddress error', err)
+    }
+  }
+
+  async getGasPrice() {
+    try {
+      const api = this.getApi()
+      return await api.getGasPrice()
+
+    } catch(err) {
+      this.log('getGasPrice error', err)
+    }
+  }
+
   /*
-  const txParams = {
-    nonce: '0x01',
-    gasPrice: '0x04e3b29200', 
-    gasLimit: '0x5208',
-    to: '0x1B8d14D5F3f0Ad4e347F4662EE3741E29D7DbAA6',
-    value: '0x0dc599702e770000', 
-    data: '',
-    // EIP 155 (chainId): mainnet: 1, ropsten: 3
-    chainId: 1
+     async function broadcastTx(from, to, raw) {
+     try {
+     const api = this.getApi()
+     log('BALANCE (from)', await api.getBalance(from))
+
+     const result = await api.sendRaw(raw)
+     console.log(result)
+
+     } catch(err) {
+     log('API ERROR', err)
+     }
+     }
+     */
+
+  getApi() {
+    const apiKey = 'TZ58GN8SI5QHUTPGFBJMMQ5JPEPIEWNQ6A'
+    return new API("https://api.etherscan.io/api", apiKey)
   }
-  */
 
-  if(!txParams['to'])
-    throw 'Missing parameter "to" in transaction params'
-
-  let tx = new EthereumTx(txParams)
-  console.log(JSON.stringify(tx))
-  return tx.serialize()
-}
-
-async function signTransaction(addressPath, txParamsStr) {
-  try {
-    let eth = await getClient()
-
-    console.log("Trying to sign the following TX:")
-    console.log(txParamsStr)
-
-    let txParams = JSON.parse(txParamsStr)
-    console.log("Parsed TX params:")
-    console.log(txParams)
-
-    let rawTransaction = generateRawTransaction(txParams)
-    console.log("Raw Transaction:")
-    console.log(rawTransaction)
-
-    let {v, r, s} = await eth.signTransaction(addressPath, rawTransaction)
-    let signedTransaction = v + r + s
-
-    console.log('--- Signed TX ---')
-    console.log(signedTransaction)
-    console.log('-----------------')
-  } catch(err) {
+  log(title, message = '') {
+    console.log(`--- ${title} ---`)
+    if(message) console.log(message)
     console.log('------------------------------')
-    console.log('--- TRANSACTION NOT SIGNED ---')
-    console.log('------------------------------')
-    console.log(err)
-    console.log('-')
+  }
+
+  printUsage(argv) {
+    let program_name = argv[0]
+    let command = argv[1]
+    console.log(`Usage: ${program_name} ${command} <options>`)
+    console.log('')
+    console.log("  --op=gen-addr|sign-tx  Operation")
+    console.log('')
+    console.log('  gen-add parameters:')
+    console.log("  --nb=N                 Number of generated addresses")
+    console.log('')
+    console.log('  get-add-from-path parameters:')
+    console.log("  --path=PATH            BIP32 path of the address")
+    console.log('')
+    console.log('  sign-tx parameters:')
+    console.log("  --addr-path=PATH       (sign-tx)  Path of private key to use")
+    console.log("  --tx-params=TXPARAMS   (sign-tx)  TX params")
   }
 }
 
-function printUsage(argv) {
-  let program_name = argv[0]
-  let command = argv[1]
-  console.log(`Usage: ${program_name} ${command} <options>`)
-  console.log('')
-  console.log("  --op=gen-addr|sign-tx  Operation")
-  console.log('')
-  console.log('  gen-add parameters:')
-  console.log("  --nb=N                 Number of generated addresses")
-  console.log('')
-  console.log('  sign-tx parameters:')
-  console.log("  --addr-path=PATH       (sign-tx)  Path of private key to use")
-  console.log("  --tx-params=TXPARAMS   (sign-tx)  TX params")
-}
+const ethwallet = new EthWallet()
+ethwallet.execute()
+
 
---
+++ src/etherscan.js
@@ -0,0 +1,120 @@
+// Polyfills
+require("babel-core/register"); // http://stackoverflow.com/a/33527883/315168
+require("babel-polyfill"); // http://stackoverflow.com/a/33527883/315168
+require('es6-promise').polyfill(); // https://github.com/matthew-andrews/isomorphic-fetch
+
+import fetch from 'isomorphic-fetch';
+import Web3 from 'web3';
+
+const web3 = new Web3();
+
+/** Throw when API call failes. */
+export class APIError extends Error {
+};
+
+
+export class API {
+
+  constructor(baseURL, apiKey) {
+    this.baseURL = baseURL;
+    this.apiKey= apiKey;
+  }
+
+  /**
+   * Perform an async HTTP request to EtherScan API
+   * @param params
+   * @returns {*}
+   */
+  async makeRequest(params) {
+
+    // http://stackoverflow.com/a/34209399/315168
+    let esc = encodeURIComponent;
+    let query = Object.keys(params)
+      .map(k => esc(k) + '=' + esc(params[k]))
+      .join('&');
+
+    let response = await fetch(this.baseURL + "?" + query);
+    let data = await response.json();
+
+    if(data.error) {
+      // {"jsonrpc":"2.0","error":{"code":-32010,"message":"Insufficient funds. Account you try to send transaction from does not have enough funds. Required 62914560000000000 and got: 37085440000000000.","data":null},"id":1}
+      throw new APIError(data.error.message);
+    }
+
+    //console.log("API result", data);
+    return data.result;
+  }
+
+  /**
+   * Return account balance as as Ether, a BigDecimal string.
+   * @param address
+   * @returns {*}
+   */
+  async getBalance(address) {
+    // https://api.etherscan.io/api?module=account&action=balance&address=0xddbd2b932c763ba5b1b7ae3b362eac3e8d40121a&tag=latest&apikey=YourApiKeyToken
+    let params = {
+      apikey: this.apiKey,
+      module: "account",
+      action: "balance",
+      address: address,
+      tag: "latest",
+    };
+    let balance = await this.makeRequest(params);
+    if(balance) {
+      return web3.utils.fromWei(balance, "ether");
+    } else {
+      return balance;
+    }
+  }
+
+  /**
+   * Get sent transaction count, including transactions in memory pool.
+   *
+   * Also can be used as a nonce.
+   *
+   * @param address
+   */
+  async getTransactionCount(address) {
+    let params = {
+      apikey: this.apiKey,
+      module: "proxy",
+      action: "eth_getTransactionCount",
+      address: address,
+      tag: "latest",
+    };
+    return await this.makeRequest(params);
+  }
+
+  /**
+   * Push out a raw signed transaction
+   *
+   * @param data Transaction as hex
+   * @return tx hash
+   */
+  async sendRaw(data) {
+
+    if(!data.startsWith("0x")) {
+      throw new Error("Data does not look like 0x hex string:" + data);
+    }
+
+    let params = {
+      apikey: this.apiKey,
+      module: "proxy",
+      action: "eth_sendRawTransaction",
+      hex: data,
+      tag: "latest",
+    };
+    return await this.makeRequest(params);
+  }
+
+  async getGasPrice() {
+
+    let params = {
+      apikey: this.apiKey,
+      module: "proxy",
+      action: "eth_gasPrice",
+    };
+    return await this.makeRequest(params);
+  }
+};
+
